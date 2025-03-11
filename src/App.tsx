3a789da05cc2ba4c36b57cb123da226109c12177
PritAm7355 <pritamtk236@gmail.com>
2025-03-11 10:52:56 +0530

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import nestjsxCrudDataProvider from "@refinedev/nestjsx-crud";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+
+function App() {
+  const API_URL = "https://api.nestjsx-crud.refine.dev";
+  const dataProvider = nestjsxCrudDataProvider(API_URL);
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            dataProvider={dataProvider}
+            routerProvider={routerBindings}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "vnPAZh-cV718p-UcLIi1",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
